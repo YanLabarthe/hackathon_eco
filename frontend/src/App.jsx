e00f084a9b60e2f9aca7f15d76a1029f7eeaada4
Yan Labarthe <yan.labarthe@gmail.com>
2022-05-13 11:00:38 +0200

    !

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import PhotoMode from "@pages/PhotoMode";
+import { useState } from "react";
 import Iphone from "@pages/Iphone";
 import "../index.css";
 
 function App() {
-  /*
-  return (
-    <div className="App h-screen text-center bg-neutral-900 text-yellow-500">
-      <Routes>
-        <Route
-          path="/"
-          element={<Home name={nameGenerated} onNameGenerated={generateName} />}
-        />
-        <Route
-          path="/quizz"
-          element={<Quizz alias={nameGenerated} onFinished={onGameEnd} />}
-        />
-        <Route
-          path="/moviequizz"
-          element={<MovieQuizz alias={nameGenerated} onFinished={onGameEnd} />}
-        />
-        <Route
-          path="/scoreboard"
-          element={
-            <Scoreboard name={nameGenerated} score={score} rank={theRank} />
-          }
-        />
-        <Route
-          path="/scoreboardMovieQuizz"
-          element={
-            <ScoreboardMovieQuizz
-              name={nameGenerated}
-              score={score}
-              rank={theRank2}
-            />
-          }
-        />
-      </Routes>
-    </div>
-  );
-  */
-
+  const [photo, setPhoto] = useState("");
   return (
     <Routes>
-      <Route path="/" element={<PhotoMode />} />
-      <Route path="/iphone" element={<Iphone />} />
+      <Route
+        path="/"
+        element={<PhotoMode photo={photo} setPhoto={setPhoto} />}
+      />
+      <Route path="/iphone" element={<Iphone photo={photo} />} />
     </Routes>
   );
 }
