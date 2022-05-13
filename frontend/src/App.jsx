4fec0c511de0aff36906230816f483f36eeb4da3
Yan Labarthe <yan.labarthe@gmail.com>
2022-05-13 15:07:05 +0200

    !

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,6 @@ import { Routes, Route } from "react-router-dom";
 
 import PhotoMode from "@pages/PhotoMode";
 import { useState } from "react";
-<<<<<<< HEAD
-import IphoneDouze from "@pages/IphoneDouze";
-
-import IphoneTreize from "@pages/IphoneTreize";
-import Marqueur from "@pages/Marqueur";
-import Chargeur from "@pages/Chargeur";
-import "../index.css";
-import Shoes from "@pages/Shoes";
-import Adaptateur from "@pages/Adaptateur";
-import RecycleVincent from "@pages/RecycleVincent";
-
-import Userprofile from "@pages/Userprofile";
-import RecycleIphoneTreize from "@pages/RecycleIphoneTreize";
-=======
 import Iphone from "@pages/Iphone";
 import Home from "@pages/Home";
 
@@ -30,15 +16,11 @@ import RecycleVincent from "@pages/RecycleVincent";
 import PhotoMode2 from "@pages/PhotoMode2";
 import PhotoMode3 from "@pages/PhotoMode2";
 
-
-
 import Userprofile from "@pages/Userprofile";
 import RecycleShoes from "@pages/RecycleShoes";
 import RecycleIphone from "@pages/recycleIphone";
 
 import AddProduct from "@pages/AddProduct";
-
->>>>>>> 392aef672df045460e9c2096ca44e589f316aade
 
 function App() {
   const [photo, setPhoto] = useState("");
@@ -46,16 +28,6 @@ function App() {
     <div className="homepage">
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-        <Route path="/vincentfile" element={<VincentFile />} />
-        <Route path="/jonathanFile" element={<JonathanFile />} />
-        <Route path="/jonathanData" element={<JonathanData />} />
-        <Route path="/iphoneTreize" element={<IphoneTreize />} />
-        <Route path="/shoes" element={<Shoes />} />
-        <Route path="/marker" element={<Marqueur />} />
-        <Route path="/photomode"element={<PhotoMode photo={photo} setPhoto={setPhoto}/>} />
-        <Route path="/iphoneDouze" element={<IphoneDouze photo={photo}  />} />
-=======
         <Route path="/vicent" element={<VincentFile />} />
         <Route path="/jonathan1" element={<JonathanFile />} />
         <Route path="/jonathan2" element={<JonathanData />} />
@@ -73,25 +45,16 @@ function App() {
           element={<PhotoMode3 photo={photo} setPhoto={setPhoto} />}
         />
         <Route path="/iphone" element={<Iphone photo={photo} />} />
->>>>>>> 392aef672df045460e9c2096ca44e589f316aade
         <Route path="/userprofile" element={<Userprofile />} />
         <Route path="/" element={<Home />} />
 
-        <Route path="/photoMode" element={<PhotoMode />} />
         <Route path="/chargeur" element={<Chargeur />} />
         <Route path="/adaptateur" element={<Adaptateur />} />
-<<<<<<< HEAD
-        <Route path="/recycleVincent" element={<RecycleVincent />} />
-        <Route path="/recycleIphoneTreize" element={<RecycleIphoneTreize/>}/>
-=======
         <Route path="/reclyclevincent" element={<RecycleVincent />} />
-        <Route path="/recycleshoes" element={<RecycleShoes/>}     />
-        <Route path="/recycleiphone"  element={<RecycleIphone/>}/>
+        <Route path="/recycleshoes" element={<RecycleShoes />} />
+        <Route path="/recycleiphone" element={<RecycleIphone />} />
 
-      
         <Route path="/addproduct" element={<AddProduct photo={photo} />} />
-
->>>>>>> 392aef672df045460e9c2096ca44e589f316aade
       </Routes>
     </div>
   );
